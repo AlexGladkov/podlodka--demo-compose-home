22a90504da9471f7a3222002c042931550b7f34f
Alex Gladkov <alexgladkov@icloud.com>
2021-04-29 18:31:57 +0300

    fix details

--- app/src/main/java/ru/agladkov/podlodka_home_task/DetailsScreen.kt
+++ app/src/main/java/ru/agladkov/podlodka_home_task/DetailsScreen.kt
@@ -3,6 +3,7 @@ package ru.agladkov.podlodka_home_task
 import android.os.Parcelable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.transform.CircleCropTransformation
 import com.google.accompanist.coil.CoilImage
 import com.google.accompanist.coil.rememberCoilPainter
 import kotlinx.android.parcel.Parcelize
@@ -31,25 +33,30 @@ data class DetailsScreenModel(
 
 @Composable
 fun DetailsScreen(model: DetailsScreenModel, navController: NavController) {
-    Box(contentAlignment = Alignment.Center) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
         Column(Modifier.padding(16.dp)) {
-            Image(
-                painter = rememberCoilPainter(
-                    request = model.avatarUrl,
-                    fadeIn = true
-                ),
-                alignment = Alignment.Center,
-                contentDescription = null,
-                modifier = Modifier.size(96.dp),
-            )
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    painter = rememberCoilPainter(
+                        request = model.avatarUrl,
+                        requestBuilder = {
+                            transformations(CircleCropTransformation())
+                        },
+                        fadeIn = true
+                    ),
+                    alignment = Alignment.Center,
+                    contentDescription = null,
+                    modifier = Modifier.size(256.dp),
+                )
+            }
 
             Text(
                 model.authorName, fontWeight = FontWeight.Medium, fontSize = 20.sp,
                 modifier = Modifier.padding(top = 16.dp).fillMaxWidth(),
                 textAlign = TextAlign.Center
             )
-            Row(modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)) {
-                Image(
+            Row(modifier = Modifier.padding(top = 32.dp, bottom = 16.dp)) {
+                Icon(
                     painterResource(R.drawable.ic_baseline_calendar_today_24),
                     "Calendar"
                 )
